620955c849c0e2ec4efdd4cd78568eb9016a0559
Sammy <101956263+samde742@users.noreply.github.com>
2025-10-20 23:13:39 -0400

    fixed lint testing errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css"
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "TRE - Next.js TypeScript Starter",
@@ -18,4 +18,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-
+}
