eef390be45e121de90cb43653a4d6e49b34f887a
Sammy <101956263+samde742@users.noreply.github.com>
2025-11-24 20:41:21 -0500

    commit for style sharing basic button placeholder WIP footer

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Button(props: {children?: React.ReactNode}) {
+    return (
+        <button className="text-body bg-btn-purple px-8 p-4 rounded-4xl">
+            {props.children}
+        </button>
+    );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Footer() {
+    <div>
+    
+    </div>
+
+}
