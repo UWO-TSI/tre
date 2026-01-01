37ddcd5fbc35ce937052b4a59e1206a9d730f61b
samdeitz <101956263+samdeitz@users.noreply.github.com>
2026-01-01 20:55:44 +0000

    Apply automatic formatting changes

--- components/accentButton/purpleAccentButton.tsx
+++ components/accentButton/purpleAccentButton.tsx
@@ -5,7 +5,9 @@ interface PurpleAccentButonProps {
 
 function PurpleAccentButton(props: PurpleAccentButonProps) {
   return (
-    <button className={`hover:bg-btn-purple/80 transition duration-100 bg-btn-purple text-white cursor-pointer rounded-full p-3 px-8 ${props.className}`}>
+    <button
+      className={`hover:bg-btn-purple/80 transition duration-100 bg-btn-purple text-white cursor-pointer rounded-full p-3 px-8 ${props.className}`}
+    >
       {props.children}
     </button>
   );
--- components/header/SearchBar.tsx
+++ components/header/SearchBar.tsx
@@ -2,17 +2,17 @@
 import { useState } from "react";
 
 export default function SearchBar() {
-    const [query, setQuery] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
 
-    return (
-        <div className="bg-btn-yellow w-full h-15">
-            <input
-                className="w-3/4 bg-white text-body-small p-2 pl-4 rounded-4xl text-[#272727]"
-                type="text"
-                placeholder="Find what you're looking for..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-        </div>
-    );
+  return (
+    <div className="bg-btn-yellow w-full h-15">
+      <input
+        className="w-3/4 bg-white text-body-small p-2 pl-4 rounded-4xl text-[#272727]"
+        type="text"
+        placeholder="Find what you're looking for..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
 }
