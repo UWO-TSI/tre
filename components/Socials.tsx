6dd01e30e617093d53f4bbe4451b9d2c67cd95af
holiazhang <zhangholia@gmail.com>
2026-01-02 16:43:19 -0500

    rel="noreferrer" on socials

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,19 +1,19 @@
 export default function Socials() {
   return (
     <div className="flex gap-2">
-      <a href="https://www.facebook.com/Childcan/" target="_blank">
+      <a href="https://www.facebook.com/Childcan/" target="_blank" rel="noreferrer">
         <img
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAyklEQVR4nO3WPWoCQRiA4S1yAisb72IhYpnjWImtF0jrEXKAXMDCkBwgNmthpYJYBh7ZYMKAJLB/swbmhWmGZb5nly0my1KpewwTbDVXXpxZBpA3OPwHUQbwVcUPWP88/w2AB0yxxh4HvMYEPLltFwWAPj6DwWdssIoFGAfDj+jVOa8K4DEAvP/yTDTAWzQAnvFSvHUAOF33irVoG3D0d8uuAfO2AUOMMAuGflz3ijVoFdD5T/hdAkgA7QByHV/JJg0jyl1KU6ksYhecC9SyYjX01QAAAABJRU5ErkJggg=="
           alt="facebook"
         />
       </a>
-      <a href="https://www.linkedin.com/in/childcan/" target="_blank">
+      <a href="https://www.linkedin.com/in/childcan/" target="_blank" rel="noreferrer">
         <img
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAA+UlEQVR4nO2WP+5BQRCAn1CoFAqF5CdxAzWFRuUALqJwChdwCjp/ChKlzg00SnRs4ffJY8g+iWLXviWyX/KKmbyZ/XaKzURRIPCNABVgCijeR0mvionADPdMTASUFBUth6j3Kkqvk0nRFS0uAA0gbymR6GdUwO3wjaRWQNa3QIMkZd8Cebl5zAjIeBWIicduc3OXAj35uhKXtFwbqAJ9YAgMgLprgTtHiWuPDCyBHUnOQMuXwJ3dq4cnbYF/oCn5jpbf+hJYa//ltFdU+RJYPNUeXtWnJTB/qt0HAX5O4F1sBNSnF5Ip7hmbLqUTh0vpGPizHGIgEKXKBfvBbtULBc+0AAAAAElFTkSuQmCC"
           alt="linkedin"
         />
       </a>
-      <a href="https://www.instagram.com/childcan/" target="_blank">
+      <a href="https://www.instagram.com/childcan/" target="_blank" rel="noreferrer">
         <img
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABo0lEQVR4nO2WTU4CQRSECSiuALfiBj2DuFG8AsgxBF0rcAp/kBOIXMNEwUSNG0X3ygFAd5jPtCnihAzj9OBEF1TSyeT1q3o1b3q6OxKZYYb/DmALaAE94IOfYXJexclNU3geaDA9To1WEAMNCQyAfSADRH3wosAKUAHepFEP0vZR8TVr9986WYeJTRtiS6SDgIWPgLKeq9Jq2gj0RMq4zC0Au0BHb2dG2xQE4sq5AA71vCqtFxsDQ5FiY/Fl4J7JuAPSY5yY5oY2Br7g8uaj4g9AHkhoFICu5m5HnfDSC2JgT+FHIOXCSTlMlMIwcK1w3oO3rZx2GAYGCic8eEnl9MMw0PdhIBWmgY7CBQ9eUTlXYRgoK9ydsAgXgWfl7IRhIK7/3OBJCy6pUXQUvxk/fIIYGE7YiNIOE24wxZd+YyPqeWzFphMl8531Z5hxadruduwG3YrPRar4Jk3WqknrzIaUE8kcNNkpiq8D79LasCXXHSaqumT4uZDE1Paao/hJEPdzDhPT4NhoWRsYwdxkzGXCLCLH3+GFoXKb1m2fYYbIH+AT1e8dKrTw7fgAAAAASUVORK5CYII="
           alt="instagram-new"
