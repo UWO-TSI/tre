c8ad016fa7811bbcb38b030baf9be56182396696
holiazhang <zhangholia@gmail.com>
2026-01-02 21:34:39 -0500

    external donate button link

--- components/DonateBar.tsx
+++ components/DonateBar.tsx
@@ -6,7 +6,13 @@ export default function DonateBar() {
       <h1 className="text-h1 text-header-purple font-light">
         When you're funding hope, every dollar counts.
       </h1>
-      <Button attributes="bg-btn-purple" text="Donate" />
+      <a 
+        href="https://form-renderer-app.donorperfect.io/give/childcan/2025-donation-form" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <Button attributes="bg-btn-purple" text="Donate" />
+      </a>
     </div>
   );
 }
