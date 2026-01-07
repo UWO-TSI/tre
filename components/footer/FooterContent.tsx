f57694163e1dad0bc98819feddc52f70156c7f6f
holiazhang <zhangholia@gmail.com>
2026-01-06 19:49:32 -0500

    highlight selection + footer copyright

--- components/footer/FooterContent.tsx
+++ components/footer/FooterContent.tsx
@@ -6,7 +6,7 @@ export default function FooterContent() {
       <div className="flex flex-col m-auto w-2/3 py-16 gap-10">
         <Contact />
         <p className="text-body-small text-footer-grey font-semibold">
-          Copyright © 2025 Childcan, The Childhood Cancer Research Association,
+          Copyright © 2026 Childcan, The Childhood Cancer Research Association,
           All Rights Reserved. <br />
           Charitable Registration #11885 1930 RR0001
         </p>
