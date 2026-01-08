b8d911a3a44bc4163b7a3416454cf3e9aa4bb184
Lucas <lucasvanderwielen@icloud.com>
2026-01-08 18:40:55 -0500

    removed dup files

--- components/Button.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button(props: { text: string; attributes: string }) {
-  return (
-    <button className={`text-body px-8 p-3 rounded-4xl ${props.attributes}`}>
-      {props.text}
-    </button>
-  );
-}
