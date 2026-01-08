06a0fc0707388d54e908d637fe58cb72548c32d6
Mohnish <mohnishpamnnai08@gmail.com>
2026-01-08 16:20:34 +0530

    Fixed the dropdown hover bug

--- components/header/headerLink.tsx
+++ components/header/headerLink.tsx
@@ -13,30 +13,32 @@ interface HeaderLinkProps {
 }
 
 function HeaderLink(props: HeaderLinkProps) {
-  const [hover, setHover] = useState(false);
+  const [titleHover, setTitleHover] = useState(false);
+  const [dropdownHover, setDropdownHover] = useState(false);
+  const showDropdown = titleHover || dropdownHover;
   return (
-    <div
-      className="flex align-middle relative z-50"
-      onPointerEnter={() => {
-        setHover(true);
-      }}
-      onPointerLeave={() => {
-        setHover(false);
-      }}
-    >
-      <div className="cursor-pointer text-body text-main-grey font-semibold mx-4 my-auto text-[18px] hover:text-header-purple transition-colors">
+    <div className="flex align-middle relative z-50">
+      <div
+        className="cursor-pointer text-body text-main-grey font-semibold mx-4 my-auto text-[18px] hover:text-header-purple transition-colors"
+        onPointerEnter={() => setTitleHover(true)}
+        onPointerLeave={() => (setTimeout(() => {
+          setTitleHover(false);
+        }, 150))}
+      >
         {props.title}
       </div>
       {props.items != undefined ? (
         <div
-          className={`${hover ? "animate-dropdown-appear" : ""} transform-[scale(1,0)] absolute flex flex-col items-start bg-white py-3 px-6 left-[-0.5em] top-10`}
+          className={`${showDropdown ? "animate-dropdown-appear" : "hidden"} transform-[scale(1,0)] absolute flex flex-col items-start bg-white py-3 px-6 left-[-0.5em] top-10`}
+          onPointerEnter={() => setDropdownHover(true)}
+          onPointerLeave={() => setDropdownHover(false)}
         >
           {props.items.map((item: HeaderLinkDropdownItem) => {
             return (
               <Link
                 key={item.link + item.label + " Drop down"}
                 href={item.link}
-                className={`text-nowrap ${hover ? "animate-dropdown-text-appear" : ""}   text-main-grey font-semibold  text-[1em] py-1 hover:text-header-purple`}
+                className={`text-nowrap ${showDropdown ? "animate-dropdown-text-appear" : ""}   text-main-grey font-semibold  text-[1em] py-1 hover:text-header-purple`}
               >
                 {item.label}
               </Link>
