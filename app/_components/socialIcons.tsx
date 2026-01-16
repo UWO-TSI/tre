3d4f697afd2fc561bd821e43120a96e78c20c386
holiazhang <194398713+holiazhang@users.noreply.github.com>
2026-01-16 04:40:55 +0000

    Apply automatic formatting changes

--- app/_components/socialIcons.tsx
+++ app/_components/socialIcons.tsx
@@ -1,28 +1,24 @@
-interface IconProps{
-    name: string;
-    link: string;
-    viewbox: string;
-    width: string;
-    height: string;
-    colour: string;
-    path: React.ReactNode;
+interface IconProps {
+  name: string;
+  link: string;
+  viewbox: string;
+  width: string;
+  height: string;
+  colour: string;
+  path: React.ReactNode;
 }
 
-export default function SocialIcons(props: IconProps){
-    return(
-    <a
-        href = {props.link}
-        target="_blank"
-        rel="noreferrer"
+export default function SocialIcons(props: IconProps) {
+  return (
+    <a href={props.link} target="_blank" rel="noreferrer">
+      <svg
+        className={`w-[${props.width}px] h-[${props.height}px] inline-block fill-${props.colour} transition duration-[170ms] ease-in-out group-hover:opacity-30 hover:opacity-100`}
+        aria-label={props.name}
+        preserveAspectRatio="xMidYMid meet"
+        viewBox={props.viewbox}
       >
-        <svg
-          className= {`w-[${props.width}px] h-[${props.height}px] inline-block fill-${props.colour} transition duration-[170ms] ease-in-out group-hover:opacity-30 hover:opacity-100`}
-          aria-label= {props.name}
-          preserveAspectRatio="xMidYMid meet"
-          viewBox= {props.viewbox}
-        >
-          {props.path}
-        </svg>
-      </a>
-    );
+        {props.path}
+      </svg>
+    </a>
+  );
 }
