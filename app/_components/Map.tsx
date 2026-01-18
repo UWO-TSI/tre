f96477d33b9afaa9b35a8889509ad083db2058df
holiazhang <194398713+holiazhang@users.noreply.github.com>
2026-01-18 20:46:42 +0000

    Apply automatic formatting changes

--- app/_components/Map.tsx
+++ app/_components/Map.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
-    
-export default function MyMap(){
-    return(
-        <APIProvider apiKey={"AIzaSyC4laW2mbAUtDCYtCVEwLqjeKEiBbWiM8Y"}>
-            <Map
-                style={{width: '626px', height: '408px'}}
-                defaultCenter={{lat: 42.952068, lng: -81.3079728}}
-                defaultZoom={12}
-                gestureHandling='greedy'
-                disableDefaultUI
-            >
-                <Marker position={{lat: 42.952068, lng: -81.3079728}} />
-            </Map>
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+
+export default function MyMap() {
+  return (
+    <APIProvider apiKey={"AIzaSyC4laW2mbAUtDCYtCVEwLqjeKEiBbWiM8Y"}>
+      <Map
+        style={{ width: "626px", height: "408px" }}
+        defaultCenter={{ lat: 42.952068, lng: -81.3079728 }}
+        defaultZoom={12}
+        gestureHandling="greedy"
+        disableDefaultUI
+      >
+        <Marker position={{ lat: 42.952068, lng: -81.3079728 }} />
+      </Map>
     </APIProvider>
-    );
+  );
 }
