7a20fc51bb2173504593b3d308ac22a89604d96a
holiazhang <194398713+holiazhang@users.noreply.github.com>
2026-01-27 22:12:42 +0000

    Apply automatic formatting changes

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           <h3 className="text-h3 pb-5 font-semibold">Looking to support?</h3>
           <YellowAccentButton>Ways to Give</YellowAccentButton>
         </div> */}
-        
+
         <QuickNavSection
           title={"Looking to support?"}
           buttonText="Ways to Give"
