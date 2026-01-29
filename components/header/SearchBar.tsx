45296a44f648dead8da0aad5d999e0fd6da3f9c3
Lucas <lucasvanderwielen@icloud.com>
2026-01-29 16:39:08 -0500

    added search magnifying glass

--- components/header/SearchBar.tsx
+++ components/header/SearchBar.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useState } from "react";
+import MagnifyingGlass from "../magnifyingGlass";
 
 export default function SearchBar() {
   const [query, setQuery] = useState<string>("");
 
   return (
-    <div className="bg-btn-yellow w-full h-15">
+    <div className="bg-btn-yellow w-full h-15 flex flex-row items-center pl-5">
+      <MagnifyingGlass></MagnifyingGlass>
       <input
-        className="w-full h-full text-body p-2 pl-6 text-black font-bold outline-none"
+        className="w-full h-full text-body p-2 pl-3 text-black font-bold outline-none"
         type="text"
         placeholder="Find what you're looking for..."
         value={query}
