5632fc9f882acfecd7b9994433dc3bb74cd145f2
Ali El-Rafih <aelrafi3@uwo.ca>
2026-01-29 15:56:05 -0500

    Fixed the dropdown hover issue (#71)
    Co-authored-by: Ali El-Rafih <aelrafih06@gmail.com>

--- components/header/headerLink.tsx
+++ components/header/headerLink.tsx
@@ -33,7 +33,7 @@ function HeaderLink(props: HeaderLinkProps) {
       </Link>
       {props.items != undefined ? (
         <div
-          className={`${hover ? "animate-dropdown-appear" : ""} transform-[scale(1,0)] absolute flex flex-col items-start bg-white py-3 px-6 left-[-0.5em] top-10`}
+          className={`${hover ? "animate-dropdown-appear" : ""} transform-[scale(1,0)] absolute flex flex-col items-start bg-white py-3 px-6 left-[-0.5em] pt-10 z-0`}
         >
           {props.items.map((item: HeaderLinkDropdownItem) => {
             return (
