5f9657a8daed07d64fcb1f4ccdf0164356aa7c87
Sam Deitz <deitsamm905@gmail.com>
2026-01-29 16:00:12 -0500

    added search results component

--- components/header/SearchBar.tsx
+++ components/header/SearchBar.tsx
@@ -1,18 +1,22 @@
 "use client";
 import { useState } from "react";
+import SearchResults from "./SearchResults";
 
 export default function SearchBar() {
   const [query, setQuery] = useState<string>("");
 
   return (
-    <div className="bg-btn-yellow w-full h-15">
-      <input
-        className="w-full h-full text-body p-2 pl-6 text-black font-bold outline-none"
-        type="text"
-        placeholder="Find what you're looking for..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+    <div className="flex flex-col">
+      <div className="bg-btn-yellow w-full h-15">
+        <input
+          className="w-full h-full text-body p-2 pl-6 text-black font-bold outline-none"
+          type="text"
+          placeholder="Find what you're looking for..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <SearchResults></SearchResults>
     </div>
   );
 }
