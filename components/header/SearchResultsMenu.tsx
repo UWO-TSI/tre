fc5ed8411cc068ed2d683a1005f0f8637e7621ad
samdeitz <101956263+samdeitz@users.noreply.github.com>
2026-01-29 22:21:19 +0000

    Apply automatic formatting changes

--- components/header/SearchResultsMenu.tsx
+++ components/header/SearchResultsMenu.tsx
@@ -6,32 +6,25 @@ export default function SearchResultsMenu(props: {
 }) {
   return (
     <div className="flex flex-col  animate-dropdown-appear w-full bg-white">
-        {
-            props.searchResults.filter((e, index) => {
-                if (index <= 3) {
-                    return e;
-                }
-            }).map((e) => {
-                return (
-                    <div key={e.route} className="flex flex-col border-b p-4">
-                        <h1 className="text-h2 text-header-purple">
-                            <PageContent pageLayout={[e.element]}></PageContent>
-                        </h1>
-                        <p className="text-[12px] text-footer-grey">
-                        {e.route}
-                        </p>
-                        <div className="text-[12px] text-footer-grey">
-                            <PageContent pageLayout={[e.layout[1]]}></PageContent>
-                        </div>
-                    </div>
-                );
-            })
-        }
-
-
-
-
-      
+      {props.searchResults
+        .filter((e, index) => {
+          if (index <= 3) {
+            return e;
+          }
+        })
+        .map((e) => {
+          return (
+            <div key={e.route} className="flex flex-col border-b p-4">
+              <h1 className="text-h2 text-header-purple">
+                <PageContent pageLayout={[e.element]}></PageContent>
+              </h1>
+              <p className="text-[12px] text-footer-grey">{e.route}</p>
+              <div className="text-[12px] text-footer-grey">
+                <PageContent pageLayout={[e.layout[1]]}></PageContent>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
