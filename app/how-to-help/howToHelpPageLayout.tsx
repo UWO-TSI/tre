fe5b16be4485ae928776d653640f6a137fa4c1fb
Sam Deitz <deitsamm905@gmail.com>
2026-01-29 20:00:49 -0500

    removed random curly braces lol

--- app/how-to-help/howToHelpPageLayout.tsx
+++ app/how-to-help/howToHelpPageLayout.tsx
@@ -392,76 +392,76 @@ export const PJDayPage: PageElement[] = [
     content: (
       <div className="flex gap-8">
         <div className="flex flex-col w-1/2 gap-4">
-          {
-            <PageContent
-              pageLayout={[
-                {
-                  type: "Header",
-                  content: <>PJ Day for Childcan.</>,
-                },
-                {
-                  type: "Subheader",
-                  content: (
-                    <>
-                      Did you know, kids fighting cancer wear their PJs every
-                      day?
-                    </>
-                  ),
-                },
-                {
-                  type: "TertiaryHeader",
-                  content: <>Show you support them on PJ Day! Rock your PJs!</>,
-                },
-                {
-                  type: "Paragraph",
-                  content: (
-                    <>
-                      Children fighting cancer spend a lot of time in their
-                      pajamas. Show your support by wearing your PJs on February
-                      15, International Childhood Cancer Awareness Day.
-                    </>
-                  ),
-                },
-                {
-                  type: "Paragraph",
-                  content: (
-                    <>
-                      As February 15, 2026, falls on a Sunday, encourage your
-                      school or business to show kids affected by cancer some
-                      love on February 13, or any day in February.
-                    </>
-                  ),
-                },
-                {
-                  type: "Button",
-                  href: "https://childcan.com/s/PJ-Day-Poster-g922.pdf",
-                  text: "Download a Poster",
-                },
-                {
-                  type: "Paragraph",
-                  content: (
-                    <>
-                      Donate to participate and help kids and their families
-                      affected by childhood cancer.
-                    </>
-                  ),
-                },
-                {
-                  type: "Paragraph",
-                  content: (
-                    <>
-                      Your involvement in PJ Day for Childcan will not only help
-                      to fund our direct-to-family assistance programs, it will
-                      also show these children and families that our community
-                      stands with them. Choose any day in February that works
-                      for your organization and let us know that you’re
-                      participating by completing this form:
-                    </>
-                  ),
-                },
-              ]}
-            ></PageContent>
-          }
+          
+          <PageContent
+            pageLayout={[
+              {
+                type: "Header",
+                content: <>PJ Day for Childcan.</>,
+              },
+              {
+                type: "Subheader",
+                content: (
+                  <>
+                    Did you know, kids fighting cancer wear their PJs every
+                    day?
+                  </>
+                ),
+              },
+              {
+                type: "TertiaryHeader",
+                content: <>Show you support them on PJ Day! Rock your PJs!</>,
+              },
+              {
+                type: "Paragraph",
+                content: (
+                  <>
+                    Children fighting cancer spend a lot of time in their
+                    pajamas. Show your support by wearing your PJs on February
+                    15, International Childhood Cancer Awareness Day.
+                  </>
+                ),
+              },
+              {
+                type: "Paragraph",
+                content: (
+                  <>
+                    As February 15, 2026, falls on a Sunday, encourage your
+                    school or business to show kids affected by cancer some
+                    love on February 13, or any day in February.
+                  </>
+                ),
+              },
+              {
+                type: "Button",
+                href: "https://childcan.com/s/PJ-Day-Poster-g922.pdf",
+                text: "Download a Poster",
+              },
+              {
+                type: "Paragraph",
+                content: (
+                  <>
+                    Donate to participate and help kids and their families
+                    affected by childhood cancer.
+                  </>
+                ),
+              },
+              {
+                type: "Paragraph",
+                content: (
+                  <>
+                    Your involvement in PJ Day for Childcan will not only help
+                    to fund our direct-to-family assistance programs, it will
+                    also show these children and families that our community
+                    stands with them. Choose any day in February that works
+                    for your organization and let us know that you’re
+                    participating by completing this form:
+                  </>
+                ),
+              },
+            ]}
+          ></PageContent>
+          
         </div>
         <div className="flex flex-col w-1/2 relative">
           <PageContent
