18cf16a8073a163968e6b0fd4311a5e3fc3e273d
Lucas <lucasvanderwielen@icloud.com>
2026-01-29 19:59:11 -0500

    added next paragraph searching for the search feature

--- components/header/SearchResultsMenu.tsx
+++ components/header/SearchResultsMenu.tsx
@@ -1,10 +1,30 @@
 import { SearchResult } from "@/hooks/useSearch";
 import PageContent from "../pageLayout/PageContent";
 import Link from "next/link";
+import { PageElement } from "../pageLayout/PageElement";
 
 export default function SearchResultsMenu(props: {
   searchResults: SearchResult[];
 }) {
+  const nextElement = (
+    ele: PageElement,
+    layout: PageElement[],
+  ): PageElement[] => {
+    const findIndex = layout.findIndex((searchEle) => {
+      return searchEle == ele;
+    });
+    if (findIndex == undefined) {
+      return [];
+    }
+    let currIndex = findIndex + 1;
+    while (currIndex < layout.length && layout[currIndex].type != "Paragraph") {
+      currIndex++;
+    }
+    if (currIndex == layout.length) {
+      return [];
+    }
+    return [layout[currIndex]];
+  };
   return (
     <div
       className={`flex flex-col absolute top-15 z-100 transition-transform ${props.searchResults.length != 0 ? "animate-dropdown-appear duration-75" : "scale-y-0"} w-full bg-white`}
@@ -26,7 +46,9 @@ export default function SearchResultsMenu(props: {
 
               <p className="text-[12px] text-foo  ter-grey">{e.route}</p>
               <div className="text-[12px] text-footer-grey">
-                <PageContent pageLayout={[e.layout[1]]}></PageContent>
+                <PageContent
+                  pageLayout={nextElement(e.element, e.layout)}
+                ></PageContent>
               </div>
             </div>
           );
