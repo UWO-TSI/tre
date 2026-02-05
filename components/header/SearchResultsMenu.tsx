0d996ee732ef1cbbdf6e4e221fb807c0ba5584ad
samdeitz <101956263+samdeitz@users.noreply.github.com>
2026-02-05 01:23:24 +0000

    Apply automatic formatting changes

--- components/header/SearchResultsMenu.tsx
+++ components/header/SearchResultsMenu.tsx
@@ -6,7 +6,7 @@ import { PageElement } from "../pageLayout/PageElement";
 export default function SearchResultsMenu(props: {
   searchResults: SearchResult[];
   isDropdownVisible: boolean;
-  dropdownRef: React.Ref<HTMLDivElement>
+  dropdownRef: React.Ref<HTMLDivElement>;
 }) {
   const nextElement = (
     ele: PageElement,
@@ -33,30 +33,30 @@ export default function SearchResultsMenu(props: {
       ref={props.dropdownRef}
       className={`flex flex-col absolute top-15 z-100 transition-transform duration-75 origin-top ${props.isDropdownVisible ? "scale-y-100" : "scale-y-0"} w-full bg-white`}
     >
-      {
-        props.searchResults.length > 0 ?
-        
-        (props.searchResults
-        .filter((e, index) => {
-          if (index < 3) {
-            return e;
-          }
-        })
-        .map((e, index) => {
-          e.element.type = "Subheader"; // make all headers look the same on the search results
-          return (
-            <div key={e.route + index} className="flex flex-col border-b p-4">
-              <Link href={e.route}>
-                <PageContent pageLayout={[e.element]}></PageContent>
-              </Link>
-              <p className="text-[12px] text-footer-grey">{e.route}</p>
-              <PageContent
-                pageLayout={nextElement(e.element, e.layout)}
-              ></PageContent>
-            </div>
-          );
-        })) : (<div className="text-secondary-grey text-body">No Results</div>)
-      }
+      {props.searchResults.length > 0 ? (
+        props.searchResults
+          .filter((e, index) => {
+            if (index < 3) {
+              return e;
+            }
+          })
+          .map((e, index) => {
+            e.element.type = "Subheader"; // make all headers look the same on the search results
+            return (
+              <div key={e.route + index} className="flex flex-col border-b p-4">
+                <Link href={e.route}>
+                  <PageContent pageLayout={[e.element]}></PageContent>
+                </Link>
+                <p className="text-[12px] text-footer-grey">{e.route}</p>
+                <PageContent
+                  pageLayout={nextElement(e.element, e.layout)}
+                ></PageContent>
+              </div>
+            );
+          })
+      ) : (
+        <div className="text-secondary-grey text-body">No Results</div>
+      )}
     </div>
   );
 }
