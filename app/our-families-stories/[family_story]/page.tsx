bb6edc518c93301fb287922b331c3db76c1579a9
samdeitz <101956263+samdeitz@users.noreply.github.com>
2026-02-06 04:14:58 +0000

    Apply automatic formatting changes

--- app/our-families-stories/[family_story]/page.tsx
+++ app/our-families-stories/[family_story]/page.tsx
@@ -6,10 +6,8 @@ export default async function Page({
 }: {
   params: Promise<{ family_story: string }>;
 }) {
-
   const { family_story } = await params;
   const familyStory: FamilyStory = familyStories[family_story];
-
 
   return (
     <div className="flex flex-col bg-white  ">
@@ -21,28 +19,27 @@ export default async function Page({
       ></HeroImage>
 
       <div className="flex flex-col gap-12 w-3/5 m-auto py-24 px-9">
-
         <p className="text-body text-footer-grey">
-          {
-            familyStory.date.toLocaleDateString("default", {
-              month: "long",
-              day: "numeric",
-              year: "numeric",
-            })
-          }
+          {familyStory.date.toLocaleDateString("default", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
         </p>
 
         <div className="flex flex-col gap-4">
-          {
-            familyStory.text.map((paragraph, index) => {
-              return (
-                <p className="text-body text-secondary-grey"  key={paragraph + index}>{paragraph}</p>
-              );
-            }) 
-          }
+          {familyStory.text.map((paragraph, index) => {
+            return (
+              <p
+                className="text-body text-secondary-grey"
+                key={paragraph + index}
+              >
+                {paragraph}
+              </p>
+            );
+          })}
         </div>
-
       </div>
     </div>
-  );  
+  );
 }
--- app/our-families-stories/story.ts
+++ app/our-families-stories/story.ts
@@ -1,22 +1,20 @@
-
 export type FamilyStory = {
-    title: string;
-    date: Date;
-    text: string[],
-    heroImage: string,
-    images: string[]
+  title: string;
+  date: Date;
+  text: string[];
+  heroImage: string;
+  images: string[];
 };
 
-
 export const familyStories: Record<string, FamilyStory> = {
-    "tara": {
-        title: 'Tara "Boom"',
-        date: new Date(2025, 11, 1),
-        text: [
-            "Some stories are etched in your heart long after you hear them. Stories of courage, love, and a kind of hope that refuses to fade, even in the face of heartbreak. This is one of those stories where sorrow is transformed into something extraordinary. ",
-            "It’s the story of a little girl named Tara “Boom” Houston. A bright, vibrant seven-year-old who adored her family, friends, and horses more than anything else in the world. It’s a story that begins with laughter and ends with lasting light and though we wish we could rewrite the middle, what remains is a legacy that continues to bring hope to countless others."
-        ],
-        heroImage: "/images/_DSC5337.JPG",
-        images: []
-    }
+  tara: {
+    title: 'Tara "Boom"',
+    date: new Date(2025, 11, 1),
+    text: [
+      "Some stories are etched in your heart long after you hear them. Stories of courage, love, and a kind of hope that refuses to fade, even in the face of heartbreak. This is one of those stories where sorrow is transformed into something extraordinary. ",
+      "It’s the story of a little girl named Tara “Boom” Houston. A bright, vibrant seven-year-old who adored her family, friends, and horses more than anything else in the world. It’s a story that begins with laughter and ends with lasting light and though we wish we could rewrite the middle, what remains is a legacy that continues to bring hope to countless others.",
+    ],
+    heroImage: "/images/_DSC5337.JPG",
+    images: [],
+  },
 };
