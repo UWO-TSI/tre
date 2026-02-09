af236336e652c3afcf23a999cb18ce326df1c579
samdeitz <101956263+samdeitz@users.noreply.github.com>
2026-02-09 15:01:49 +0000

    Apply automatic formatting changes

--- components/embeds/FacebookVideoEmbed.tsx
+++ components/embeds/FacebookVideoEmbed.tsx
@@ -5,26 +5,27 @@ import { useEffect } from "react";
 type EmbedType = "video" | "page";
 let sdkLoaded = false;
 
-export default function FacebookVideoEmbed(props: { url: string; type: EmbedType }) {
+export default function FacebookVideoEmbed(props: {
+  url: string;
+  type: EmbedType;
+}) {
   useEffect(() => {
-        if (!window.FB) {
+    if (!window.FB) {
+      if (!sdkLoaded) {
+        sdkLoaded = true;
 
-            if (!sdkLoaded) {
-                sdkLoaded = true;
-
-                const script = document.createElement("script");
-                script.src =
-                    "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v17.0";
-                script.async = true;
-                script.defer = true;
-                script.onload = () => window.FB?.XFBML.parse();
-                document.body.appendChild(script);
-            }
-            
-        } else {
-            window.FB?.XFBML.parse();
-        }
-    }, []);
+        const script = document.createElement("script");
+        script.src =
+          "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v17.0";
+        script.async = true;
+        script.defer = true;
+        script.onload = () => window.FB?.XFBML.parse();
+        document.body.appendChild(script);
+      }
+    } else {
+      window.FB?.XFBML.parse();
+    }
+  }, []);
 
   return (
     <div className="w-full aspect-video">
