4ce50f0a7708602a44da7b2744eaab84b4ff4617
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2026-02-13 11:52:08 -0500

    sanity fixes

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,28 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.ctctcdn.com https://static-tracking.klaviyo.com",
+              "style-src 'self' 'unsafe-inline' https://static.ctctcdn.com",
+              "img-src 'self' data: https: blob:",
+              "font-src 'self' data:",
+              "connect-src 'self' https://static.ctctcdn.com https://*.constantcontact.com",
+              "frame-src 'self' https://static.ctctcdn.com https://*.constantcontact.com",
+              "form-action 'self' https://*.constantcontact.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
