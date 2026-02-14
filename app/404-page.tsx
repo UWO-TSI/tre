79ad8cd7a73c96425bbd6b5695db4ceece99ae5e
AliElrafih <182176729+AliElrafih@users.noreply.github.com>
2026-02-14 08:05:16 +0000

    Apply automatic formatting changes

--- app/404-page.tsx
+++ app/404-page.tsx
@@ -3,7 +3,6 @@ import YellowAccentButton from "@/othername";
 export default function NotFound() {
   return (
     <div style={{ display: "flex", height: "100vh" }}>
-
       {/* Image on left side */}
       <div style={{ width: "50%" }}>
         <img
@@ -12,7 +11,7 @@ export default function NotFound() {
           style={{
             width: "100%",
             height: "100%",
-            objectFit: "cover"
+            objectFit: "cover",
           }}
         />
       </div>
@@ -25,16 +24,15 @@ export default function NotFound() {
           padding: "60px",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center"
+          justifyContent: "center",
         }}
       >
-
         <img
           src="/images/Childcan-Logo.png.webp"
           alt="Childcan Logo"
           style={{
             width: "235px",
-            marginBottom: "100px"
+            marginBottom: "100px",
           }}
         />
 
@@ -42,7 +40,7 @@ export default function NotFound() {
           style={{
             fontSize: "80px",
             color: "#514776",
-            margin: 0
+            margin: 0,
           }}
         >
           404!
@@ -53,18 +51,15 @@ export default function NotFound() {
             marginTop: "20px",
             marginBottom: "30px",
             color: "#555",
-            maxWidth: "400px"
+            maxWidth: "400px",
           }}
         >
-          We can't find the page you're looking for, sorry about that. It has either moved, or does not exist.
+          We can't find the page you're looking for, sorry about that. It has
+          either moved, or does not exist.
         </p>
 
-        <YellowAccentButton>
-          Home Page
-        </YellowAccentButton>
-
+        <YellowAccentButton>Home Page</YellowAccentButton>
       </div>
-
     </div>
   );
 }
